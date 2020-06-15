d99c651263bd93b57bc8b9320be7fd58ec1c7852
Miguel Antunes <miguel.aka.kelter@gmail.com>
2020-06-15 12:04:18 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const pup = puppeteer.launch({
-    headless: false, // debug only
+    headless: true, // debug only
     args: ['--no-sandbox']
     //userDataDir: './user_data'
 });
